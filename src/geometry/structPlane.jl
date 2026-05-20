ae70800f4bbb894f342e5da1be1bf2a63f9b859c
ArthurJWH <arthurjwhwang@gmail.com>
2026-05-20 16:56:26 -0400

    Added simple constructors

--- src/geometry/structPlane.jl
+++ src/geometry/structPlane.jl
@@ -10,6 +10,19 @@ struct Plane
     alpha_stall::Float64
     beta_stall::Float64
 
-    ME::Float64
+    SM::Float64
     alpha_trim::Float64
+end
+
+function Plane(surfaces::Vector{Surface})
+
+    coeffs = Coeffs()
+    CG = (0.0, 0.0, 0.0)
+    MTOW = 0.0
+    alpha_stall = 0.0
+    beta_stall = 0.0
+    SM = 0.0
+    alpha_trim = 0.0
+
+    return Plane(surfaces, coeffs, CG, MTOW, alpha_stall, beta_stall, SM, alpha_trim)
 end
--- src/geometry/structSurface.jl
+++ src/geometry/structSurface.jl
@@ -1,3 +1,7 @@
+include("../../assets/airfoils/Plain/Plain.jl")
+
+using ..Utils
+
 mutable struct Surface{chordF,twistF,sweepF,dihedralF}
     # y is the spanwise fraction coordinate
     # x is the chordwise fraction coordinate
@@ -10,8 +14,9 @@ mutable struct Surface{chordF,twistF,sweepF,dihedralF}
     b::Float64
     S::Float64
     AR::Float64
+    MGC::Float64
     MAC::Float64
-    Swet::Float64
+    # Swet::Float64 future drag build up
 
     ys::Vector{Float64}
     airfoils::Vector{String}
@@ -25,4 +30,31 @@ mutable struct Surface{chordF,twistF,sweepF,dihedralF}
     sw_center::Float64
     dihedral::dihedralF
     
+end
+
+function Surface(;
+    name::String="Surface",
+    mirror_xz::Bool=true,
+    pos::Tuple{Float64, Float64, Float64}=(0.0, 0.0, 0.0),
+    rot::Tuple{Float64, Float64, Float64}=(0.0, 0.0, 0.0),
+    b::Float64=1.0,
+    ys::Vector{Float64}=[0.0, 1.0],
+    airfoils::Vector{Airfoil}=[plain, plain],
+    chord::chordF=y -> 1.0,
+    twist::twistF=y -> 0.0,
+    tw_center::Float64=0.25,
+    sweep::sweepF=y -> 0.0,
+    sw_center::Float64=0.25,
+    dihedral::dihedralF=y -> 0.0
+    ) where {chordF, twistF, sweepF, dihedralF}
+
+    MGC = IntegrateGLQ(chord)(0, 1)
+    S = MGC * b
+    AR = b^2 / S
+    MAC = 2 * IntegrateGLQ(x -> chord(x)^2)(0, 1) / MGC # Assume wing is symmetric about the centerline
+    return Surface{chordF, twistF, sweepF, dihedralF}(name, mirror_xz, pos, rot,
+                                                      b, S, AR, MGC, MAC, ys,
+                                                      airfoils, chord, twist,
+                                                      tw_center, sweep, sw_center,
+                                                      dihedral)
 end
