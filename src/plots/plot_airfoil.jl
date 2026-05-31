e97fb0d8dcd99e48b611246618cc257a1d2f0cb0
ArthurJWH <arthurjwhwang@gmail.com>
2026-05-31 10:14:10 -0400

    New airfoils added

--- src/plots/plot_airfoil.jl
+++ src/plots/plot_airfoil.jl
@@ -10,13 +10,13 @@ function plot_airfoil(airfoil::Airfoil; save = false)
 
     title = airfoil.name
 
-    p = plot(x, y_top, label = "Top Surface", title = title, xlabel = "x", ylabel = "y", aspect_ratio = :equal)
-    plot!(p, x, y_bottom, label = "Bottom Surface")
-    plot!(p, x, y_camber, label = "Camber Line", linestyle = :dash)
+    p = Plots.plot(x, y_top, label = "Top Surface", title = title, xlabel = "x", ylabel = "y", aspect_ratio = :equal)
+    Plots.plot!(p, x, y_bottom, label = "Bottom Surface")
+    Plots.plot!(p, x, y_camber, label = "Camber Line", linestyle = :dash)
 
     if save
         save_path = replace(airfoil.datfile, ".dat" => ".png")
-        savefig(p, save_path)
+        Plots.savefig(p, save_path)
     end
 
     return p
